d65d0a2b28c46c4ff14e25d4f370d30b016385af
hqlulu <hqlulu@gmail.com>
2019-08-13 08:03:49 +0800

    basic pom setting

---
+++ src/com/lhq/basic/Test.java
@@ -0,0 +1,11 @@
+package com.lhq.basic;
+
+/**
+ * @author hqlulu
+ * @date 2019/8/13 上午8:00
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.print("test");
+    }
+}
